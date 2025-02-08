aea2ffcda9451480d9ba0b15d8001f8443a06ee8
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-08 19:14:38 +0600

    show random alphabet in the display

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,9 +1,12 @@
 function continueGame(){
-    const alphabet = getRandomAlphabet()
+    const alphabet = getRandomAlphabet();
+
+    const currentAlphabet = document.getElementById("current-alphabet");
+    currentAlphabet.innerText = alphabet;
 }
 
 function play(){
-    hideElementById("home-screen")
-    showElementById("play-ground")
-    continueGame()
+    hideElementById("home-screen");
+    showElementById("play-ground");
+    continueGame();
 }
